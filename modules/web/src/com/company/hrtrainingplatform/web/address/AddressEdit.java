254677c95a122b367c403c56815721f1ab21db06
brentvanisterbecq <brent.vanisterbecq@student.ehb.be>
2017-11-14 17:04:51 +0100

    Location & Address UI/VIEWS added
    Met Amir gemaakt. (Locatie/address kunnen aangemaakt worden)

---
+++ modules/web/src/com/company/hrtrainingplatform/web/address/AddressEdit.java
@@ -0,0 +1,7 @@
+package com.company.hrtrainingplatform.web.address;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.company.hrtrainingplatform.entity.Address;
+
+public class AddressEdit extends AbstractEditor<Address> {
+}
