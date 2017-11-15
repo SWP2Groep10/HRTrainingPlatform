85e589d2287c114526d7b2228a836ddc2d1403a1
NsrWissam <wissam.nasser@student.ehb.be>
2017-11-15 16:20:54 +0100

    Revert "Certificate view/UI"
    This reverts commit 93402f219ab0dd3d9460d15e809bdf2a4e1cef44.

--- modules/core/db/update/mysql/17/171115-2-updateAddress02.sql
+++
@@ -1,4 +0,0 @@
-alter table HRTRAININGPLATFORM_ADDRESS drop column POSTALCODE cascade ;
-alter table HRTRAININGPLATFORM_ADDRESS add column POSTALCODE varchar(255) ^
-update HRTRAININGPLATFORM_ADDRESS set POSTALCODE = '' where POSTALCODE is null ;
-alter table HRTRAININGPLATFORM_ADDRESS modify column POSTALCODE varchar(255) not null ;
