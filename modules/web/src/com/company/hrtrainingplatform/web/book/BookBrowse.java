7a658f53cfbe1d7de788b1e6af123844a057e8dd
NsrWissam <wissam.nasser@student.ehb.be>
2017-11-15 12:58:00 +0100

    Excel button added to books view
    that converts table into excel sheet

--- modules/web/src/com/company/hrtrainingplatform/web/book/BookBrowse.java
+++ modules/web/src/com/company/hrtrainingplatform/web/book/BookBrowse.java
@@ -3,4 +3,7 @@ package com.company.hrtrainingplatform.web.book;
 import com.haulmont.cuba.gui.components.EntityCombinedScreen;
 
 public class BookBrowse extends EntityCombinedScreen {
+
+    public void onExcelBtnClick() {
+    }
 }
