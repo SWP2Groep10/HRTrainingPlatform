c08734d1b52b71f029060a4ef5e0ae784d5786ef
TomVanlaerEHB <32544414+TomVanlaerEHB@users.noreply.github.com>
2017-11-27 10:13:37 +0100

    Revert "Tom"

--- modules/web/src/com/company/hrtrainingplatform/web/employee/EmployeeBrowse.java
+++ modules/web/src/com/company/hrtrainingplatform/web/employee/EmployeeBrowse.java
@@ -1,31 +1,6 @@
 package com.company.hrtrainingplatform.web.employee;
 
 import com.haulmont.cuba.gui.components.EntityCombinedScreen;
-import com.haulmont.cuba.gui.components.Component;
 
-import java.io.*;
-import java.net.URL;
-import java.nio.charset.Charset;
-import org.json.*;
-
-public class EmployeeBrowse extends EntityCombinedScreen{
-    public void onOData(Component source)throws IOException{
-        URL oData = new URL("http://services.odata.org/V3/Northwind/Northwind.svc/Employees?$format=json");
-        InputStream is = oData.openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            StringBuilder sb = new StringBuilder();
-            int cp;
-            while ((cp = rd.read()) != -1) {
-                sb.append((char) cp);
-            }
-            String jsonText = sb.toString();
-            JSONObject json = new JSONObject(jsonText);
-            showNotification(json.toString());
-        } catch(Exception e) {
-            showNotification(e.getMessage());
-        } finally {
-            is.close();
-        }
-    }
+public class EmployeeBrowse extends EntityCombinedScreen {
 }
