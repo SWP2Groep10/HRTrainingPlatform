dfa93a827216b038731eac49410983b6f74f76cb
TomVanlaerEHB <tom.vanlaer@student.ehb.be>
2017-12-04 21:02:35 +0100

    Commit to pull

--- modules/web/src/com/company/hrtrainingplatform/web/employee/EmployeeBrowse.java
+++ modules/web/src/com/company/hrtrainingplatform/web/employee/EmployeeBrowse.java
@@ -11,7 +11,12 @@ import org.json.*;
 public class EmployeeBrowse extends EntityCombinedScreen{
     public void onOData(Component source)throws IOException{
         URL oData = new URL("http://services.odata.org/V3/Northwind/Northwind.svc/Employees?$format=json");
-        InputStream is = oData.openStream();
+        JSONObject oDataJSON = importJSON(oData);
+        showNotification(oDataJSON.toString());
+    }
+
+    public JSONObject importJSON(URL url)throws IOException{
+        InputStream is = url.openStream();
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             StringBuilder sb = new StringBuilder();
@@ -21,11 +26,13 @@ public class EmployeeBrowse extends EntityCombinedScreen{
             }
             String jsonText = sb.toString();
             JSONObject json = new JSONObject(jsonText);
-            showNotification(json.toString());
+            return json;
         } catch(Exception e) {
             showNotification(e.getMessage());
         } finally {
             is.close();
         }
+        return null;
     }
+
 }
