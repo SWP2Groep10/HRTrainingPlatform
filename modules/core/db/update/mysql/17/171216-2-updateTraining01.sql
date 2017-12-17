f61c09d4236be7dc88c2c3e25cd05643f465beb8
NsrWissam <wissam.nasser@student.ehb.be>
2017-12-18 00:25:48 +0100

    Revert "revert to test branch"
    This reverts commit 8e033cc7f0b920f097633d051fd60624e428a643.

--- modules/core/db/update/mysql/17/171216-2-updateTraining01.sql
+++
@@ -1,4 +0,0 @@
--- alter table HRTRAININGPLATFORM_TRAINING add column MANAGER_ID varchar(32) ^
--- update HRTRAININGPLATFORM_TRAINING set MANAGER_ID = <default_value> ;
--- alter table HRTRAININGPLATFORM_TRAINING modify column MANAGER_ID varchar(32) not null ;
-alter table HRTRAININGPLATFORM_TRAINING add column MANAGER_ID varchar(32) not null ;
