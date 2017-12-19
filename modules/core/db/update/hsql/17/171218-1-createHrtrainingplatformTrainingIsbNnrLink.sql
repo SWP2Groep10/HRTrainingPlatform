bef696044221da5230ec129c80615f0737fec5f9
FemkeKruf <femkekruf@hotmail.com>
2017-12-19 12:35:20 +0100

    test fix fcking manager <3

--- modules/core/db/update/hsql/17/171218-1-createHrtrainingplatformTrainingIsbNnrLink.sql
+++
@@ -1,5 +0,0 @@
-create table HRTRAININGPLATFORM_TRAINING_ISB_NNR_LINK (
-    TRAINING_ID varchar(36) not null,
-    I_S_B_NNR_ID varchar(36) not null,
-    primary key (TRAINING_ID, I_S_B_NNR_ID)
-);
