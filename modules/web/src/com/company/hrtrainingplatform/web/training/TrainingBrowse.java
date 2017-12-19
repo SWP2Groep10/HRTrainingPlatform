1dd13eef8bd6a4ceb2ac88322f30dea2dd5c3fe2
brentvanisterbecq <brent.vanisterbecq@student.ehb.be>
2017-12-19 15:35:29 +0100

    Commit Location search
    Code werkt niet -> Wissam checkt dit

--- modules/web/src/com/company/hrtrainingplatform/web/training/TrainingBrowse.java
+++ modules/web/src/com/company/hrtrainingplatform/web/training/TrainingBrowse.java
@@ -1,11 +1,32 @@
 package com.company.hrtrainingplatform.web.training;
 
+import com.company.hrtrainingplatform.entity.Location;
+import com.company.hrtrainingplatform.entity.Training;
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.components.AbstractWindow;
 import com.haulmont.cuba.gui.components.AbstractLookup;
 import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.Table;
+
+import javax.inject.Inject;
 
 public class TrainingBrowse extends AbstractLookup {
 
+  @Inject
+  private Table<Training> trainingsTable;
+
+
     public void onExcel(Component source) {
     }
+
+
+
+    public void onShowLocationClick() {
+        Training t = trainingsTable.getSingleSelected();
+        trainingsTable.setEnabled(true);
+       // String url = "http://maps.google.com/?q=" + t.getAddress().getCountry() + "+" + a.getAddress().getCity() + "+" + a.getAddress().getStreet() + "+" + a.getAddress().getNumber();
+        String url = "http://maps.google.com/?q=" + t.getLocation().getLocationName();
+        showWebPage(url, ParamsMap.of("target", "_blank"));
+    }
+
 }
