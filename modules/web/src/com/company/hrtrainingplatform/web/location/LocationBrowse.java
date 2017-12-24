64baad96631b3ef0801ceb06b2f890c398366058
NsrWissam <wissam.nasser@student.ehb.be>
2017-12-24 14:57:04 +0100

    (realy) fixed bug + added google maps button to location browse screen

--- modules/web/src/com/company/hrtrainingplatform/web/location/LocationBrowse.java
+++ modules/web/src/com/company/hrtrainingplatform/web/location/LocationBrowse.java
@@ -1,6 +1,26 @@
 package com.company.hrtrainingplatform.web.location;
 
+import com.company.hrtrainingplatform.entity.Address;
+import com.company.hrtrainingplatform.entity.Location;
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.components.EntityCombinedScreen;
+import com.haulmont.cuba.gui.components.Table;
+
+import javax.inject.Inject;
 
 public class LocationBrowse extends EntityCombinedScreen {
+
+    @Inject
+    private Table<Location> table;
+
+  public void onShowMapClick() {
+        try {
+            Location l = table.getSingleSelected();
+            Address a = l.getAddress();
+            String url = "http://maps.google.com/?q=" + a.getCountry() + "+" + a.getCity() + "+" + a.getPostalcode() + "+" + a.getStreet() + "+" + a.getNumber();
+            showWebPage(url, ParamsMap.of("target", "_blank"));
+        }
+        catch (NullPointerException e){showNotification("No selection has been made.");}
+    }
+    
 }
